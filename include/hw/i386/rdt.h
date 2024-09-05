b5e5990aa7eee33458247b07bcce2d0c3be4cfbe
‪Hendrik Wüthrich <whendrik@google.com>
2024-09-05 11:22:32 +0000

    i386: Add RDT functionality
    Add RDT code to Associate CLOSID with RMID / set RMID for monitoring,
    write COS, and read monitoring data. This patch does not add code for
    the guest to interact through these things with MSRs, only the actual
    ability for the RDT device to do them.
    
    Signed-off-by: Hendrik Wüthrich <whendrik@google.com>

--- include/hw/i386/rdt.h
+++ include/hw/i386/rdt.h
@@ -17,9 +17,25 @@
 #ifndef HW_RDT_H
 #define HW_RDT_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 typedef struct RDTState RDTState;
 typedef struct RDTStatePerCore RDTStatePerCore;
 typedef struct RDTMonitor RDTMonitor;
 typedef struct RDTAllocation RDTAllocation;
+
+bool rdt_associate_rmid_cos(uint64_t msr_ia32_pqr_assoc);
+
+void rdt_write_msr_l3_mask(uint32_t pos, uint32_t val);
+void rdt_write_msr_l2_mask(uint32_t pos, uint32_t val);
+void rdt_write_mba_thrtl(uint32_t pos, uint32_t val);
+
+uint32_t rdt_read_l3_mask(uint32_t pos);
+uint32_t rdt_read_l2_mask(uint32_t pos);
+uint32_t rdt_read_mba_thrtl(uint32_t pos);
+
+uint64_t rdt_read_event_count(RDTStatePerCore *rdt, uint32_t rmid, uint32_t event_id);
+uint32_t rdt_max_rmid(RDTStatePerCore *rdt);
 
 #endif
