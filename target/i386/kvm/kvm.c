58f0b9423e32e412617f00e6313d72b7f59cadce
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-09-10 10:40:12 -0700

    target/i386: fix build warning (gcc-12 -fsanitize=thread)
    Found on debian stable.
    
    ../target/i386/kvm/kvm.c: In function ‘kvm_handle_rdmsr’:
    ../target/i386/kvm/kvm.c:5345:1: error: control reaches end of non-void function [-Werror=return-type]
     5345 | }
          | ^
    ../target/i386/kvm/kvm.c: In function ‘kvm_handle_wrmsr’:
    ../target/i386/kvm/kvm.c:5364:1: error: control reaches end of non-void function [-Werror=return-type]
     5364 | }
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -5770,7 +5770,7 @@ static int kvm_handle_rdmsr(X86CPU *cpu, struct kvm_run *run)
         }
     }
 
-    assert(false);
+    g_assert_not_reached();
 }
 
 static int kvm_handle_wrmsr(X86CPU *cpu, struct kvm_run *run)
@@ -5789,7 +5789,7 @@ static int kvm_handle_wrmsr(X86CPU *cpu, struct kvm_run *run)
         }
     }
 
-    assert(false);
+    g_assert_not_reached();
 }
 
 static bool has_sgx_provisioning;
