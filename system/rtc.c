d86fe0cf831ae8bfa151fac1289e1961f5b517eb
Daniel P. Berrangé <berrange@redhat.com>
2024-10-17 12:33:35 +0100

    system: adapt to new import path for qobject data type headers
    The qobject data type headers have moved from qapi/qmp/ to
    qobject/.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- system/rtc.c
+++ system/rtc.c
@@ -25,6 +25,7 @@
 #include "qemu/osdep.h"
 #include "qemu/cutils.h"
 #include "qapi/error.h"
+#include "qapi/qmp/qerror.h"
 #include "qemu/error-report.h"
 #include "qemu/option.h"
 #include "qemu/timer.h"
