162ccff5fcf066c8ae1dde23c98d5c50dfae41ea
fuqiang wang <wangfuqiang49@jd.com>
2024-11-22 01:34:16 +0800

    fix dirtyring not converge use small dirtylimit
    When using a low dirtylimit to restrict a VM with a high dirty ratio,
    such as in the guestperf test scenario compr-dirty-limit-period-1000
    (dirtylimit=1), this can lead to too long sleep times for the vcpu
    threads and the migration thread may have to wait vcpu thread due to the
    kick operation, and during this waiting period, the guest will make some
    dirty pages at a high speed, which causes convergence issues.
    
    However, during the vcpu thread sleeping, it is not in kernel space
    (kvm), so there is no need to wait for a kick.
    
    Signed-off-by: wangfuqiang49 <wangfuqiang49@jd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -926,7 +926,9 @@ static void kvm_cpu_synchronize_kick_all(void)
     CPUState *cpu;
 
     CPU_FOREACH(cpu) {
-        run_on_cpu(cpu, do_kvm_cpu_synchronize_kick, RUN_ON_CPU_NULL);
+        if (cpu->vcpu_in_kvm) {
+            run_on_cpu(cpu, do_kvm_cpu_synchronize_kick, RUN_ON_CPU_NULL);
+        }
     }
 }
 
@@ -3131,7 +3133,9 @@ int kvm_cpu_exec(CPUState *cpu)
          */
         smp_rmb();
 
+        cpu->vcpu_in_kvm = true;
         run_ret = kvm_vcpu_ioctl(cpu, KVM_RUN, 0);
+        cpu->vcpu_in_kvm = false;
 
         attrs = kvm_arch_post_run(cpu, run);
 
