f4d6609d8fbd4db27072e0774c43f86a5c686666
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-11-25 19:56:17 +0000

    hw/arm/boot: Mark all guest memory as RIPAS_RAM.
    All Realm IPA states are by default RIPAS_EMPTY, and accessing them in
    that state causes injection of synchronous exception. Either the loader
    or the guest needs to set IPA state to RIPAS_RAM before accessing it.
    Since a Linux guest needs all memory ready at boot [1], initialize it
    here.
    
    [1] https://docs.kernel.org/arch/arm64/booting.html
        https://lore.kernel.org/all/20241004144307.66199-12-steven.price@arm.com/
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- hw/arm/boot.c
+++ hw/arm/boot.c
@@ -26,6 +26,7 @@
 #include "qemu/config-file.h"
 #include "qemu/option.h"
 #include "qemu/units.h"
+#include "kvm_arm.h"
 
 /* Kernel boot protocol is specified in the kernel docs
  * Documentation/arm/Booting and Documentation/arm64/booting.txt
@@ -1237,6 +1238,9 @@ void arm_load_kernel(ARMCPU *cpu, MachineState *ms, struct arm_boot_info *info)
     info->initrd_filename = ms->initrd_filename;
     info->dtb_filename = ms->dtb;
     info->dtb_limit = 0;
+
+    /* Mark all Realm memory as RAM */
+    kvm_arm_rme_init_guest_ram(info->loader_start, info->ram_size);
 
     /* Load the kernel.  */
     if (!info->kernel_filename || info->firmware_loaded) {
