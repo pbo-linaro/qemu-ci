a1f6aa761f5ac717f2b3cec2d0c4693b723e1d27
Bibo Mao <maobibo@loongson.cn>
2024-11-28 10:10:22 +0800

    hw/intc/loongarch_ipi: Add cpu map table from physical cpu id
    Bitmap table present_cpu_map is added, it is to allocate logical cpu
    when CPU object is created. Also present_cpu array is added to get
    logical cpu from physical cpu id.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>

--- include/hw/intc/loongarch_ipi.h
+++ include/hw/intc/loongarch_ipi.h
@@ -10,16 +10,22 @@
 
 #include "qom/object.h"
 #include "hw/intc/loongson_ipi_common.h"
+#include "hw/loongarch/virt.h"
 
+#define INVALID_CPU         -1
 #define TYPE_LOONGARCH_IPI  "loongarch_ipi"
 OBJECT_DECLARE_TYPE(LoongarchIPIState, LoongarchIPIClass, LOONGARCH_IPI)
 
 struct LoongarchIPIState {
     LoongsonIPICommonState parent_obj;
+    DECLARE_BITMAP(present_cpu_map, LOONGARCH_MAX_CPUS);
+    int present_cpu[MAX_PHY_ID];
+    CPUState *cs[MAX_PHY_ID];
 };
 
 struct LoongarchIPIClass {
     LoongsonIPICommonClass parent_class;
+    DeviceRealize parent_realize;
 };
 
 #endif
