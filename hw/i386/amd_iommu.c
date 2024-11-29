qemu/qemu

commit b12cb3819baf6d9ee8140d4dd6d36fa829e2c6d9

amd_iommu: Check APIC ID > 255 for XTSup
The XTSup mode enables x2APIC support for AMD IOMMU, which is needed
to support vcpu w/ APIC ID > 255.

Reviewed-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
Signed-off-by: Santosh Shukla <santosh.shukla@amd.com>
Message-Id: <20240927172913.121477-6-santosh.shukla@amd.com>
Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

hw/i386/amd_iommu.c
@@ -32,6 +32,7 @@
 #include "trace.h"
 #include "hw/i386/apic-msidef.h"
 #include "hw/qdev-properties.h"
+#include "kvm/kvm_i386.h"
 
 /* used AMD-Vi MMIO registers */
 const char *amdvi_mmio_low[] = {
@@ -1650,6 +1651,16 @@ static void amdvi_sysbus_realize(DeviceState *dev, Error **errp)
                           s, "amdvi-ir", AMDVI_INT_ADDR_SIZE);
     memory_region_add_subregion_overlap(&s->mr_sys, AMDVI_INT_ADDR_FIRST,
                                         &s->mr_ir, 1);
+
+    /* AMD IOMMU with x2APIC mode requires xtsup=on */
+    if (x86ms->apic_id_limit > 255 && !s->xtsup) {
+        error_report("AMD IOMMU with x2APIC confguration requires xtsup=on");
+        exit(EXIT_FAILURE);
+    }
+    if (s->xtsup && kvm_irqchip_is_split() && !kvm_enable_x2apic()) {
+        error_report("AMD IOMMU xtsup=on requires support on the KVM side");
+        exit(EXIT_FAILURE);
+    }
 
     pci_setup_iommu(bus, &amdvi_iommu_ops, s);
     amdvi_init(s);

pbo-linaro/qemu-ci

commit c1f612c53075e8645060c3bb4a406b60897224f5

hw/i386/amd_iommu: Fix XTSup feature check when KVM is not available
When building with debug on a host which doesn't provide KVM,
we get:

    C compiler for the host machine: clang (clang 16.0.0 "Apple clang
    version 16.0.0 (clang-1600.0.26.4)")
    C linker for the host machine: clang ld64 1115.7.3
    Host machine cpu family: aarch64
    Host machine cpu: aarch64
       Compilation
         host CPU                        : aarch64
         host endianness                 : little
         C compiler                      : clang
         Host C compiler                 : clang
         C++ compiler                    : NO
         Objective-C compiler            : clang
         Rust support                    : NO
         CFLAGS                          : -g -O0
       User defined options
         optimization                    : 0
    ...
    [1589/1590] Linking target qemu-system-x86_64-unsigned
    Undefined symbols for architecture arm64:
       "_kvm_enable_x2apic", referenced from:
           _amdvi_sysbus_realize in hw_i386_amd_iommu.c.o
    ld: symbol(s) not found for architecture arm64

In commit 9926cf34de5 ("target/i386: Allow elision of
kvm_enable_x2apic()") we removed this symbol stub globally
(debug and optimized build profiles). All code wanted to
access it must be protected by a check on kvm_enabled().
See the similar check in x86_cpus_init() added by commit
c04cfb4596a ("hw/i386: fix short-circuit logic with
non-optimizing builds").

In order to fix this linking error, protect the whole
block checking the XTSup feature with a check on whether
KVM is enabled.

Since x86_cpus_init() already checks APIC ID > 255 imply
kernel support for irqchip and X2APIC, remove the confuse
and unlikely reachable "AMD IOMMU xtsup=on requires support
on the KVM side" message.

Fix a type in "configuration" in error message.

Fixes: b12cb3819baf (amd_iommu: Check APIC ID > 255 for XTSup)
Reported-by: Phil Dennis-Jordan <phil@philjordan.eu>

hw/i386/amd_iommu.c
@@ -1652,13 +1652,8 @@ static void amdvi_sysbus_realize(DeviceState *dev, Error **errp)
     memory_region_add_subregion_overlap(&s->mr_sys, AMDVI_INT_ADDR_FIRST,
                                         &s->mr_ir, 1);
 
-    /* AMD IOMMU with x2APIC mode requires xtsup=on */
-    if (x86ms->apic_id_limit > 255 && !s->xtsup) {
-        error_report("AMD IOMMU with x2APIC confguration requires xtsup=on");
-        exit(EXIT_FAILURE);
-    }
-    if (s->xtsup && kvm_irqchip_is_split() && !kvm_enable_x2apic()) {
-        error_report("AMD IOMMU xtsup=on requires support on the KVM side");
+    if (kvm_enabled() && x86ms->apic_id_limit > 255 && !s->xtsup) {
+        error_report("AMD IOMMU with x2APIC configuration requires xtsup=on");
         exit(EXIT_FAILURE);
     }
 