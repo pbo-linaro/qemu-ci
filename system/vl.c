79df4a884f3fcfd4fa3cc726d5d02f93fe9a3d64
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-12-03 10:21:51 +0100

    accel/tcg: Remove mentions of legacy '-machine foo,accel=bar'
    Since commit 6f6e1698a68 ("vl: configure accelerators from -accel
    options") we prefer the '-accel bar' command line option.
    
    Update the documentation when TCG is referred to.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- system/vl.c
+++ system/vl.c
@@ -2385,7 +2385,7 @@ static void configure_accelerators(const char *progname)
         for (tmp = accel_list; *tmp; tmp++) {
             /*
              * Filter invalid accelerators here, to prevent obscenities
-             * such as "-machine accel=tcg,,thread=single".
+             * such as "-accel tcg,,thread=single".
              */
             if (accel_find(*tmp)) {
                 qemu_opts_parse_noisily(qemu_find_opts("accel"), *tmp, true);
