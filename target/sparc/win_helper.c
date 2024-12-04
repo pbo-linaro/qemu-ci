c872a7f1b45cba80a163bc7a510f4ff2c3a632e7
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-12-04 21:41:51 +0100

    target/sparc: Use memcpy() in memcpy32()
    Rather than manually copying each register, use
    the libc memcpy(), which is well optimized.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- target/sparc/win_helper.c
+++ target/sparc/win_helper.c
@@ -26,14 +26,7 @@
 
 static inline void memcpy32(target_ulong *dst, const target_ulong *src)
 {
-    dst[0] = src[0];
-    dst[1] = src[1];
-    dst[2] = src[2];
-    dst[3] = src[3];
-    dst[4] = src[4];
-    dst[5] = src[5];
-    dst[6] = src[6];
-    dst[7] = src[7];
+    memcpy(dst, src, 8 * sizeof(target_ulong));
 }
 
 void cpu_set_cwp(CPUSPARCState *env, int new_cwp)
