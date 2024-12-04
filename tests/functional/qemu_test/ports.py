81d4e3b37de68172317508ced8fa9d6764bf9d5d
Thomas Huth <thuth@redhat.com>
2024-12-04 08:19:08 +0100

    tests/functional: Extract the find_free_ports() function into a helper file
    We'll need this functionality in other functional tests, too, so
    let's extract it into the qemu_test module.
    Also add  an __enter__ and __exit__ function that can be used for
    using this functionality in a locked context, so that tests that
    are running in parallel don't try to compete for the same ports
    later.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>

---
+++ tests/functional/qemu_test/ports.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+#
+# Simple functional tests for VNC functionality
+#
+# Copyright 2018, 2024 Red Hat, Inc.
+#
+# This work is licensed under the terms of the GNU GPL, version 2 or
+# later.  See the COPYING file in the top-level directory.
+
+import fcntl
+import os
+import socket
+import sys
+import tempfile
+from typing import List
+
+class Ports():
+
+    PORTS_ADDR = '127.0.0.1'
+    PORTS_START = 32768
+    PORTS_END = PORTS_START + 1024
+
+    def __enter__(self):
+        lock_file = os.path.join(tempfile.gettempdir(), "qemu_port_lock")
+        self.lock_fh = os.open(lock_file, os.O_CREAT)
+        fcntl.flock(self.lock_fh, fcntl.LOCK_EX)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        fcntl.flock(self.lock_fh, fcntl.LOCK_UN)
+        os.close(self.lock_fh)
+
+    def check_bind(self, port: int) -> bool:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                sock.bind((self.PORTS_ADDR, port))
+            except OSError:
+                return False
+
+        return True
+
+    def find_free_ports(self, count: int) -> List[int]:
+        result = []
+        for port in range(self.PORTS_START, self.PORTS_END):
+            if self.check_bind(port):
+                result.append(port)
+                if len(result) >= count:
+                    break
+        assert len(result) == count
+        return result
+
+    def find_free_port(self) -> int:
+        return self.find_free_ports(1)[0]
