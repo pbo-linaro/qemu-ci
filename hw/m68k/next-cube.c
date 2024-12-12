afe49b738616f08ee1317a03084a7ec3a21cad9d
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2024-12-12 11:46:20 +0000

    next-cube: replace boiler-plate GPL 2.0 or later license text with SPDX identifier
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- hw/m68k/next-cube.c
+++ hw/m68k/next-cube.c
@@ -4,10 +4,7 @@
  * Copyright (c) 2011 Bryce Lanham
  * Copyright (c) 2024 Mark Cave-Ayland
  *
- * This code is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 #include "qemu/osdep.h"
