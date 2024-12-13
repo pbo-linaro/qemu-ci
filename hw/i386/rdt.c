71ce1a24600e487055056ebfca0aa75b798b5d44
‪Hendrik Wüthrich <whendrik@google.com>
2024-12-13 17:26:41 +0000

    i386: Add RDT device interface through MSRs
    Implement rdmsr and wrmsr for the following MSRs:
    * MSR_IA32_PQR_ASSOC
    * MSR_IA32_QM_EVTSEL
    * MSR_IA32_QM_CTR
    * IA32_L3_QOS_Mask_n
    * IA32_L2_QOS_Mask_n
    * IA32_L2_QoS_Ext_BW_Thrtl_n
    
    This allows for the guest to call RDT-internal functions to
    associate an RMID with a CLOSID / set an active RMID for
    monitoring, read monitoring data, and set classes of service.
    
    Signed-off-by: Hendrik Wüthrich <whendrik@google.com>

--- hw/i386/rdt.c
+++ hw/i386/rdt.c
@@ -81,6 +81,10 @@ struct RDTState {
 struct RDTStateClass {
 };
 
+uint32_t rdt_get_cpuid_10_1_edx_cos_max(void) { return RDT_MAX_L3_MASK_COUNT; }
+uint32_t rdt_get_cpuid_10_2_edx_cos_max(void) { return RDT_MAX_L2_MASK_COUNT; }
+uint32_t rdt_get_cpuid_10_3_edx_cos_max(void) { return RDT_MAX_MBA_THRTL_COUNT; }
+
 bool rdt_associate_rmid_cos(uint64_t msr_ia32_pqr_assoc)
 {
     X86CPU *cpu = X86_CPU(current_cpu);
