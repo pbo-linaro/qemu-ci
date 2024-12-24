5869c1671e478220fbdc258a48d8f299f194607e
Liu Jaloo <liu.jaloo@gmail.com>
2024-12-24 11:27:36 +0800

    macaddr: ignore the checking from index to 0xFF
    bash-5.1$ git diff net/net.c

--- net/net.c
+++ net/net.c
@@ -173,6 +173,7 @@ static void qemu_macaddr_set_used(MACAddr *macaddr)
     for (index = 0x56; index < 0xFF; index++) {
         if (macaddr->a[5] == index) {
             mac_table[index]++;
+           return;
         }
     }
 }
@@ -188,6 +189,7 @@ static void qemu_macaddr_set_free(MACAddr *macaddr)
     for (index = 0x56; index < 0xFF; index++) {
         if (macaddr->a[5] == index) {
             mac_table[index]--;
+           return;
         }
     }
 }
