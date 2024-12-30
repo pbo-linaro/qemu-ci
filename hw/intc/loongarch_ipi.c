33eefb66fafbe410ff9ebc1b4a3f5f0b6d8d6677
Bibo Mao <maobibo@loongson.cn>
2024-12-30 09:34:01 +0800

    hw/intc/loongarch_ipi: Optimize function cpu_by_arch_id
    There is cpu mapping from physical cpu id inside, cpu_by_arch_id
    can be optimized from cpu mapping table.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>

--- hw/intc/loongarch_ipi.c
+++ hw/intc/loongarch_ipi.c
@@ -17,49 +17,24 @@ static AddressSpace *get_iocsr_as(CPUState *cpu)
     return LOONGARCH_CPU(cpu)->env.address_space_iocsr;
 }
 
-static int archid_cmp(const void *a, const void *b)
-{
-   CPUArchId *archid_a = (CPUArchId *)a;
-   CPUArchId *archid_b = (CPUArchId *)b;
-
-   return archid_a->arch_id - archid_b->arch_id;
-}
-
-static CPUArchId *find_cpu_by_archid(MachineState *ms, uint32_t id)
-{
-    CPUArchId apic_id, *found_cpu;
-
-    apic_id.arch_id = id;
-    found_cpu = bsearch(&apic_id, ms->possible_cpus->cpus,
-                        ms->possible_cpus->len,
-                        sizeof(*ms->possible_cpus->cpus),
-                        archid_cmp);
-
-    return found_cpu;
-}
-
 static int loongarch_cpu_by_arch_id(LoongsonIPICommonState *lics,
                                     int64_t arch_id, int *index, CPUState **pcs)
 {
-    MachineState *machine = MACHINE(qdev_get_machine());
-    CPUArchId *archid;
-    CPUState *cs;
+    LoongarchIPIState *lis = LOONGARCH_IPI(lics);
 
-    archid = find_cpu_by_archid(machine, arch_id);
-    if (archid && archid->cpu) {
-        cs = archid->cpu;
-        if (index) {
-            *index = cs->cpu_index;
-        }
-
-        if (pcs) {
-            *pcs = cs;
-        }
-
-        return MEMTX_OK;
+    if ((arch_id >= MAX_PHY_ID) || (lis->devs[arch_id].cs == NULL)) {
+        return MEMTX_ERROR;
     }
 
-    return MEMTX_ERROR;
+    if (index) {
+        *index = lis->devs[arch_id].index;
+    }
+
+    if (pcs) {
+        *pcs = lis->devs[arch_id].cs;
+    }
+
+    return MEMTX_OK;
 }
 
 static void loongarch_cpu_plug(HotplugHandler *hotplug_dev,
