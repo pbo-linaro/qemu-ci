c5650dfb3538d1b4f263143ec750d76951357f76
Richard Henderson <richard.henderson@linaro.org>
2025-01-02 10:06:20 -0800

    tcg/s390x: Fold the ext{8, 16, 32}[us] cases into {s}extract
    Accept byte and word extensions with the extract opcodes.
    This is preparatory to removing the specialized extracts.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/s390x/tcg-target-has.h
+++ tcg/s390x/tcg-target-has.h
@@ -57,7 +57,7 @@ extern uint64_t s390_facilities[3];
 #define TCG_TARGET_HAS_ext16u_i32     1
 #define TCG_TARGET_HAS_deposit_i32    1
 #define TCG_TARGET_HAS_extract_i32    1
-#define TCG_TARGET_HAS_sextract_i32   0
+#define TCG_TARGET_HAS_sextract_i32   1
 #define TCG_TARGET_HAS_extract2_i32   0
 #define TCG_TARGET_HAS_extr_i64_i32   0
 #define TCG_TARGET_HAS_qemu_st8_i32   0
@@ -70,7 +70,7 @@ extern uint64_t s390_facilities[3];
 #define TCG_TARGET_HAS_ext32u_i64     1
 #define TCG_TARGET_HAS_deposit_i64    1
 #define TCG_TARGET_HAS_extract_i64    1
-#define TCG_TARGET_HAS_sextract_i64   0
+#define TCG_TARGET_HAS_sextract_i64   1
 #define TCG_TARGET_HAS_extract2_i64   0
 
 #define TCG_TARGET_HAS_qemu_ldst_i128 1
@@ -95,5 +95,23 @@ extern uint64_t s390_facilities[3];
 #define TCG_TARGET_HAS_bitsel_vec     1
 #define TCG_TARGET_HAS_cmpsel_vec     1
 #define TCG_TARGET_HAS_tst_vec        0
+
+#define TCG_TARGET_extract_valid(type, ofs, len)   1
+
+static inline bool
+tcg_target_sextract_valid(TCGType type, unsigned ofs, unsigned len)
+{
+    if (ofs == 0) {
+        switch (len) {
+        case 8:
+        case 16:
+            return true;
+        case 32:
+            return type == TCG_TYPE_I64;
+        }
+    }
+    return false;
+}
+#define TCG_TARGET_sextract_valid  tcg_target_sextract_valid
 
 #endif
