9f4fe06f12d24ddcbcb188929fadf31c8fbb60c8
Peter Xu <peterx@redhat.com>
2025-01-02 22:17:55 +0100

    qdev: Add machine_get_container()
    Add a helper to fetch machine containers.  Add some sanity check around.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-ID: <20241121192202.4155849-10-peterx@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- include/hw/qdev-core.h
+++ include/hw/qdev-core.h
@@ -1038,6 +1038,16 @@ Object *qdev_get_machine(void);
 Object *qdev_create_fake_machine(void);
 
 /**
+ * machine_get_container:
+ * @name: The name of container to lookup
+ *
+ * Get a container of the machine (QOM path "/machine/NAME").
+ *
+ * Returns: the machine container object.
+ */
+Object *machine_get_container(const char *name);
+
+/**
  * qdev_get_human_name() - Return a human-readable name for a device
  * @dev: The device. Must be a valid and non-NULL pointer.
  *
