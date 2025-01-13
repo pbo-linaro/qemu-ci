e03e7d952aa6564638713e91247587da71905499
Bibo Mao <maobibo@loongson.cn>
2025-01-13 16:25:55 +0800

    target/loongarch: Add common header file for CSR register
    Common header file csr.h is added here, it can be used by both
    TCG mode and kvm mode.
    
    Macro CONFIG_TCG is used for TCG specific CSR function because
    of  function parameter such as TCGv and TCGv_ptr.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>

---
+++ target/loongarch/csr.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2025 Loongson Technology Corporation Limited
+ */
+
+#ifndef TARGET_LOONGARCH_CSR_H
+#define TARGET_LOONGARCH_CSR_H
+
+#include "cpu-csr.h"
+#ifdef CONFIG_TCG
+#include "tcg/tcg-op.h"
+#endif
+
+#ifdef CONFIG_TCG
+typedef void (*GenCSRRead)(TCGv dest, TCGv_ptr env);
+typedef void (*GenCSRWrite)(TCGv dest, TCGv_ptr env, TCGv src);
+#else
+typedef void (*GenCSRRead)(void);
+typedef void (*GenCSRWrite)(void);
+#endif
+
+enum {
+    CSRFL_READONLY = (1 << 0),
+    CSRFL_EXITTB   = (1 << 1),
+    CSRFL_IO       = (1 << 2),
+};
+
+typedef struct {
+    int offset;
+    int flags;
+    GenCSRRead readfn;
+    GenCSRWrite writefn;
+} CSRInfo;
+
+#endif /* TARGET_LOONGARCH_TCG_LOONGARCH_H */
