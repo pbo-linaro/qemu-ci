b956c63637d4ddd9abe3e47e681d84fd85e5b264
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-01-16 00:29:42 +0100

    hw/mips/loongson3_virt: Invert vCPU creation order to remove &first_cpu
    Create vCPUs from the last one to the first one.
    No need to use the &first_cpu global since we already
    have it referenced.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- hw/mips/loongson3_virt.c
+++ hw/mips/loongson3_virt.c
@@ -494,7 +494,6 @@ static void mips_loongson3_virt_init(MachineState *machine)
     long bios_size;
     MIPSCPU *cpu;
     Clock *cpuclk;
-    CPUMIPSState *env;
     DeviceState *liointc;
     DeviceState *ipi = NULL;
     char *filename;
@@ -569,7 +568,7 @@ static void mips_loongson3_virt_init(MachineState *machine)
     cpuclk = clock_new(OBJECT(machine), "cpu-refclk");
     clock_set_hz(cpuclk, DEF_LOONGSON3_FREQ);
 
-    for (i = 0; i < machine->smp.cpus; i++) {
+    for (i = machine->smp.cpus - 1; i >= 0; --i) {
         int node = i / LOONGSON3_CORE_PER_NODE;
         int core = i % LOONGSON3_CORE_PER_NODE;
         int ip;
@@ -609,7 +608,6 @@ static void mips_loongson3_virt_init(MachineState *machine)
                                pin, cpu->env.irq[ip + 2]);
         }
     }
-    env = &MIPS_CPU(first_cpu)->env;
 
     /* Allocate RAM/BIOS, 0x00000000~0x10000000 is alias of 0x80000000~0x90000000 */
     memory_region_init_rom(bios, NULL, "loongson3.bios",
@@ -640,7 +638,7 @@ static void mips_loongson3_virt_init(MachineState *machine)
         loaderparams.kernel_filename = kernel_filename;
         loaderparams.kernel_cmdline = kernel_cmdline;
         loaderparams.initrd_filename = initrd_filename;
-        loaderparams.kernel_entry = load_kernel(env);
+        loaderparams.kernel_entry = load_kernel(&cpu->env);
 
         init_boot_rom();
         init_boot_param();
