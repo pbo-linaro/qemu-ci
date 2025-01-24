0577ff8d98771c308a1e56d30afe0a8dbfa7ef0c
Thomas Huth <thuth@redhat.com>
2025-01-24 15:15:27 +0100

    tests/functional/test_mipsel_malta: Convert the mipsel replay tests
    Move the mipsel replay tests from tests/avocado/replay_kernel.py to
    the functional framework. Since the functional tests should be run per
    target, we cannot stick all replay tests in one file. Thus let's add
    these tests to the file where we already use the same asset already.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- tests/functional/test_mipsel_malta.py
+++ tests/functional/test_mipsel_malta.py
@@ -9,12 +9,13 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-from qemu_test import QemuSystemTest, LinuxKernelTest, Asset
+from qemu_test import QemuSystemTest, Asset, wait_for_console_pattern
 from qemu_test import interrupt_interactive_console_until_pattern
-from qemu_test import wait_for_console_pattern
+from qemu_test import skipLongRuntime
+from replay_kernel import ReplayKernelBase
 
 
-class MaltaMachineConsole(LinuxKernelTest):
+class MaltaMachineConsole(ReplayKernelBase):
 
     ASSET_KERNEL_4K = Asset(
         ('http://mipsdistros.mips.com/LinuxDistro/nanomips/'
@@ -56,6 +57,29 @@ class MaltaMachineConsole(LinuxKernelTest):
 
     def test_mips_malta32el_nanomips_64k_dbg(self):
         self.do_test_mips_malta32el_nanomips(self.ASSET_KERNEL_64K)
+
+    def do_test_replay_mips_malta32el_nanomips(self, kernel_asset):
+        self.set_machine('malta')
+        self.cpu = 'I7200'
+        kernel_path = self.uncompress(kernel_asset)
+
+        kernel_command_line = (self.KERNEL_COMMON_COMMAND_LINE +
+                               'mem=256m@@0x0 '
+                               'console=ttyS0')
+        console_pattern = 'Kernel command line: %s' % kernel_command_line
+        self.run_rr(kernel_path, kernel_command_line, console_pattern, shift=5)
+
+    @skipLongRuntime()
+    def test_replay_mips_malta32el_nanomips_4k(self):
+        self.do_test_replay_mips_malta32el_nanomips(self.ASSET_KERNEL_4K)
+
+    @skipLongRuntime()
+    def test_replay_mips_malta32el_nanomips_16k_up(self):
+        self.do_test_replay_mips_malta32el_nanomips(self.ASSET_KERNEL_16K)
+
+    @skipLongRuntime()
+    def test_replay_mips_malta32el_nanomips_64k_dbg(self):
+        self.do_test_replay_mips_malta32el_nanomips(self.ASSET_KERNEL_64K)
 
 
 class MaltaMachineYAMON(QemuSystemTest):
