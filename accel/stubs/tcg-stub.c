cf0837edab6145b71c86ca9c26604058929ad34a
Richard Henderson <richard.henderson@linaro.org>
2025-02-02 19:18:14 -0800

    accel/stubs: Expand stubs for TCG
    Add tcg_allowed, qmp_x_query_jit, qmp_x_query_opcount.
    These are referenced when CONFIG_TCG is enabled globally,
    but not for a specific target.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- accel/stubs/tcg-stub.c
+++ accel/stubs/tcg-stub.c
@@ -13,6 +13,18 @@
 #include "qemu/osdep.h"
 #include "exec/tb-flush.h"
 #include "exec/exec-all.h"
+#include "qapi/error.h"
+
+/*
+ * This file *ought* to be built once and linked only when required.
+ * However, it is built per-target, which means qemu/osdep.h has already
+ * undef'ed CONFIG_TCG, which hides the auto-generated declaration.
+ */
+#define CONFIG_TCG
+#include "qapi/qapi-commands-machine.h"
+
+
+const bool tcg_allowed = false;
 
 void tb_flush(CPUState *cpu)
 {
@@ -26,4 +38,16 @@ G_NORETURN void cpu_loop_exit(CPUState *cpu)
 G_NORETURN void cpu_loop_exit_restore(CPUState *cpu, uintptr_t pc)
 {
     g_assert_not_reached();
+}
+
+HumanReadableText *qmp_x_query_jit(Error **errp)
+{
+    error_setg(errp, "JIT information is only available with accel=tcg");
+    return NULL;
+}
+
+HumanReadableText *qmp_x_query_opcount(Error **errp)
+{
+    error_setg(errp, "Opcode count information is only available with accel=tcg");
+    return NULL;
 }
