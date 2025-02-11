c4c2fd6640fa42b126546efa552ff4704b890dab
Daniil Tatianin <d-tatianin@yandex-team.ru>
2025-02-11 17:50:46 -0500

    os: add an ability to lock memory on_fault
    This will be used in the following commits to make it possible to only
    lock memory on fault instead of right away.
    
    Reviewed-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Daniil Tatianin <d-tatianin@yandex-team.ru>
    Link: https://lore.kernel.org/r/20250123131944.391886-2-d-tatianin@yandex-team.ru
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- os-posix.c
+++ os-posix.c
@@ -327,18 +327,24 @@ void os_set_line_buffering(void)
     setvbuf(stdout, NULL, _IOLBF, 0);
 }
 
-int os_mlock(void)
+int os_mlock(bool on_fault)
 {
 #ifdef HAVE_MLOCKALL
     int ret = 0;
+    int flags = MCL_CURRENT | MCL_FUTURE;
 
-    ret = mlockall(MCL_CURRENT | MCL_FUTURE);
+    if (on_fault) {
+        flags |= MCL_ONFAULT;
+    }
+
+    ret = mlockall(flags);
     if (ret < 0) {
         error_report("mlockall: %s", strerror(errno));
     }
 
     return ret;
 #else
+    (void)on_fault;
     return -ENOSYS;
 #endif
 }
