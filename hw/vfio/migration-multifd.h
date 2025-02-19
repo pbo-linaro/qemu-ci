d624750de65a13eef283fad15967805f09a805d3
Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
2025-02-19 21:34:17 +0100

    vfio/migration: Add x-migration-load-config-after-iter VFIO property
    This property allows configuring whether to start the config load only
    after all iterables were loaded.
    Such interlocking is required for ARM64 due to this platform VFIO
    dependency on interrupt controller being loaded first.
    
    The property defaults to AUTO, which means ON for ARM, OFF for other
    platforms.
    
    Signed-off-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>

--- hw/vfio/migration-multifd.h
+++ hw/vfio/migration-multifd.h
@@ -22,8 +22,11 @@ bool vfio_multifd_transfer_enabled(VFIODevice *vbasedev);
 
 bool vfio_multifd_transfer_setup(VFIODevice *vbasedev, Error **errp);
 
+bool vfio_load_config_after_iter(VFIODevice *vbasedev);
 bool vfio_load_state_buffer(void *opaque, char *data, size_t data_size,
                             Error **errp);
+
+int vfio_load_state_config_load_ready(VFIODevice *vbasedev);
 
 void vfio_multifd_emit_dummy_eos(VFIODevice *vbasedev, QEMUFile *f);
 
