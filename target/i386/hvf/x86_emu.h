bdeaa8f37ec138b86ed492f807a51d04f60cb789
Wei Liu <liuwe@linux.microsoft.com>
2025-02-21 00:36:19 -0800

    target/i386/hvf: move and rename simulate_{rdmsr, wrmsr}
    This requires making raise_exception non-static. That function needs to be
    renamed to avoid clashing with a function in TCG.
    
    Mostly code movement. No functional change.
    
    Signed-off-by: Wei Liu <liuwe@linux.microsoft.com>

--- target/i386/hvf/x86_emu.h
+++ target/i386/hvf/x86_emu.h
@@ -36,9 +36,7 @@ extern const struct x86_emul_ops *emul_ops;
 
 void init_emu(const struct x86_emul_ops *ops);
 bool exec_instruction(CPUX86State *env, struct x86_decode *ins);
-
-void simulate_rdmsr(CPUX86State *env);
-void simulate_wrmsr(CPUX86State *env);
+void x86_emul_raise_exception(CPUX86State *env, int exception_index, int error_code);
 
 target_ulong read_reg(CPUX86State *env, int reg, int size);
 void write_reg(CPUX86State *env, int reg, target_ulong val, int size);
