1de9978464ce8b46e782cfd5ab8665d70438129b
Jamin Lin via <qemu-devel@nongnu.org>
2025-03-03 17:54:31 +0800

    hw/intc/aspeed: Reduce regs array size by adding a register sub-region
    Currently, the size of the "regs" array is 0x2000, which is too large. So far,
    it only uses "GICINT128 to `GICINT134", and the offsets from 0 to 0x1000 are
    unused. To save code size and avoid mapping large unused gaps, update to only
    map the useful set of registers:
    
    INTC register [0x1000 – 0x1804]
    
    Update "reg_size" to 0x808. Introduce a new class attribute "reg_offset" to set
    the start offset of a "INTC" sub-region. Set the "reg_offset" to 0x1000 for INTC
    registers.
    
    Signed-off-by: Jamin Lin <jamin_lin@aspeedtech.com>

--- include/hw/intc/aspeed_intc.h
+++ include/hw/intc/aspeed_intc.h
@@ -16,7 +16,7 @@
 #define TYPE_ASPEED_2700_INTC TYPE_ASPEED_INTC "-ast2700"
 OBJECT_DECLARE_TYPE(AspeedINTCState, AspeedINTCClass, ASPEED_INTC)
 
-#define ASPEED_INTC_NR_REGS (0x2000 >> 2)
+#define ASPEED_INTC_NR_REGS (0x808 >> 2)
 #define ASPEED_INTC_NR_INTS 9
 
 struct AspeedINTCState {
@@ -43,6 +43,7 @@ struct AspeedINTCClass {
     uint32_t num_ints;
     uint64_t mem_size;
     uint64_t reg_size;
+    uint64_t reg_offset;
 };
 
 #endif /* ASPEED_INTC_H */
