63e7d32fac5eaf67c1530c160afb153de006b86d
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-03-09 00:09:15 +0100

    hw/vfio/s390x: Compile AP and CCW once
    Since the files don't use any target-specific knowledge anymore,
    move them to system_ss[] to build them once, even if they are
    only used for one unique binary (qemu-system-s390x).
    
    Because files in system_ss[] don't get the target/foo/ path in
    their CPPFLAGS, use header paths relative to the root directory.
    
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- hw/vfio/ap.c
+++ hw/vfio/ap.c
@@ -23,7 +23,7 @@
 #include "qemu/module.h"
 #include "qemu/option.h"
 #include "qemu/config-file.h"
-#include "kvm/kvm_s390x.h"
+#include "target/s390x/kvm/kvm_s390x.h"
 #include "migration/vmstate.h"
 #include "hw/qdev-properties.h"
 #include "hw/s390x/ap-bridge.h"
