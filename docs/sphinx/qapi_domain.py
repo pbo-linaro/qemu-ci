46aac2c97f9c94fb2c9fc14760fb62a69f9b9f9f
John Snow <jsnow@redhat.com>
2025-03-09 04:35:07 -0400

    docs/qapi-domain: add qapi:alternate directive
    Add the .. qapi:alternate:: directive, object, and qapi:alt:`name`
    cross-reference role.
    
    Add the "Alternatives:" field list for describing alternate choices. Like
    other field lists that reference QAPI types, a forthcoming commit will
    add cross-referencing support to this field.
    
    Signed-off-by: John Snow <jsnow@redhat.com>

--- docs/sphinx/qapi_domain.py
+++ docs/sphinx/qapi_domain.py
@@ -324,6 +324,23 @@ class QAPIEnum(QAPIObject):
     )
 
 
+class QAPIAlternate(QAPIObject):
+    """Description of a QAPI Alternate."""
+
+    doc_field_types = QAPIObject.doc_field_types.copy()
+    doc_field_types.extend(
+        [
+            # :alt type name: descr
+            TypedField(
+                "alternative",
+                label=_("Alternatives"),
+                names=("alt",),
+                can_collapse=False,
+            ),
+        ]
+    )
+
+
 class QAPIModule(QAPIDescription):
     """
     Directive to mark description of a new module.
@@ -451,6 +468,7 @@ class QAPIDomain(Domain):
         "module": ObjType(_("module"), "mod", "any"),
         "command": ObjType(_("command"), "cmd", "any"),
         "enum": ObjType(_("enum"), "enum", "type", "any"),
+        "alternate": ObjType(_("alternate"), "alt", "type", "any"),
     }
 
     # Each of these provides a rST directive,
@@ -459,6 +477,7 @@ class QAPIDomain(Domain):
         "module": QAPIModule,
         "command": QAPICommand,
         "enum": QAPIEnum,
+        "alternate": QAPIAlternate,
     }
 
     # These are all cross-reference roles; e.g.
@@ -468,6 +487,7 @@ class QAPIDomain(Domain):
         "mod": QAPIXRefRole(),
         "cmd": QAPIXRefRole(),
         "enum": QAPIXRefRole(),
+        "alt": QAPIXRefRole(),
         # reference any data type (excludes modules, commands, events)
         "type": QAPIXRefRole(),
         "any": QAPIXRefRole(),  # reference *any* type of QAPI object.
