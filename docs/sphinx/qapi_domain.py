0aeb20161324be0e472e5389b3c5d2a58c21d446
John Snow <jsnow@redhat.com>
2025-03-09 04:35:01 -0400

    docs/qapi-domain: add :since: directive option
    Add a little special markup for registering "Since:" information. Adding
    it as an option instead of generic content lets us hoist the information
    into the Signature bar, optionally put it in the index, etc.
    
    Signed-off-by: John Snow <jsnow@redhat.com>

--- docs/sphinx/qapi_domain.py
+++ docs/sphinx/qapi_domain.py
@@ -178,6 +178,8 @@ class QAPIObject(QAPIDescription):
         {
             # Borrowed from the Python domain:
             "module": directives.unchanged,  # Override contextual module name
+            # These are QAPI originals:
+            "since": directives.unchanged,
         }
     )
 
@@ -191,7 +193,17 @@ class QAPIObject(QAPIDescription):
 
     def get_signature_suffix(self) -> List[nodes.Node]:
         """Returns a suffix to put after the object name in the signature."""
-        return []
+        ret: List[nodes.Node] = []
+
+        if "since" in self.options:
+            ret += [
+                SpaceNode(" "),
+                addnodes.desc_sig_element(
+                    "", f"(Since: {self.options['since']})"
+                ),
+            ]
+
+        return ret
 
     def handle_signature(self, sig: str, signode: desc_signature) -> Signature:
         """
