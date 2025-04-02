qemu/qemu

commit c740ad92d0d958fa785e5d7aa1b67ecaf30a6a54

QSLIST: add lock-free operations
These operations are trivial to implement and do not have ABA problems.
They are enough to implement simple multiple-producer, single consumer
lock-free lists or, as in the next patch, the multiple consumers can
steal a whole batch of elements and process them at their leisure.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
Reviewed-by: Fam Zheng <famz@redhat.com>
Message-id: 1417518350-6167-5-git-send-email-pbonzini@redhat.com
Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

include/qemu/queue.h
@@ -191,8 +191,19 @@ struct {                                                                \
 } while (/*CONSTCOND*/0)
 
 #define QSLIST_INSERT_HEAD(head, elm, field) do {                        \
-        (elm)->field.sle_next = (head)->slh_first;                      \
-        (head)->slh_first = (elm);                                      \
+        (elm)->field.sle_next = (head)->slh_first;                       \
+        (head)->slh_first = (elm);                                       \
+} while (/*CONSTCOND*/0)
+
+#define QSLIST_INSERT_HEAD_ATOMIC(head, elm, field) do {                   \
+        do {                                                               \
+            (elm)->field.sle_next = (head)->slh_first;                     \
+        } while (atomic_cmpxchg(&(head)->slh_first, (elm)->field.sle_next, \
+                               (elm)) != (elm)->field.sle_next);           \
+} while (/*CONSTCOND*/0)
+
+#define QSLIST_MOVE_ATOMIC(dest, src) do {                               \
+        (dest)->slh_first = atomic_xchg(&(src)->slh_first, NULL);        \
 } while (/*CONSTCOND*/0)
 
 #define QSLIST_REMOVE_HEAD(head, field) do {                             \

pbo-linaro/qemu-ci

commit b84549317e24e28ab0b69ebdb221f0f2bfaf377f

Fix data race with slh_first field in test-aio-multithread
This patch addresses potential data races involving access to the
slh_first field in the QSLIST_INSERT_HEAD_ATOMIC macro.

Fixes: c740ad92d0 ("QSLIST: add lock-free operations")
Resolves: https://gitlab.com/qemu-project/qemu/-/issues/2902
Signed-off-by: Vitalii Mordan <mordan@ispras.ru>

include/qemu/queue.h
@@ -217,7 +217,8 @@ struct {                                                                \
 #define QSLIST_INSERT_HEAD_ATOMIC(head, elm, field) do {                     \
         typeof(elm) save_sle_next;                                           \
         do {                                                                 \
-            save_sle_next = (elm)->field.sle_next = (head)->slh_first;       \
+            save_sle_next = qatomic_read(&(head)->slh_first);                \
+            (elm)->field.sle_next = save_sle_next;                           \
         } while (qatomic_cmpxchg(&(head)->slh_first, save_sle_next, (elm)) !=\
                  save_sle_next);                                             \
 } while (/*CONSTCOND*/0)