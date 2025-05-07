b71c033cda1449996b25c56a8f08feb1f2ab92b4
Gustavo Romero <gustavo.romero@linaro.org>
2025-05-07 20:46:25 +0000

    Fix typo in comments
    Fix the duplicate "the" article in various comments.
    
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>
    Suggested-by: Thomas Huth <thuth@redhat.com>

--- block.c
+++ block.c
@@ -3153,8 +3153,8 @@ bdrv_attach_child_common(BlockDriverState *child_bs,
      * stop new requests from coming in. This is fine, we don't care about the
      * old requests here, they are not for this child. If another place enters a
      * drain section for the same parent, but wants it to be fully quiesced, it
-     * will not run most of the the code in .drained_begin() again (which is not
-     * a problem, we already did this), but it will still poll until the parent
+     * will not run most of the code in .drained_begin() again (which is not a
+     * problem, we already did this), but it will still poll until the parent
      * is fully quiesced, so it will not be negatively affected either.
      */
     bdrv_parent_drained_begin_single(new_child);
