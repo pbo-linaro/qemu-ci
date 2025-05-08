7f4db608126626cac7208f8f901a46a2b421ca5a
Fiona Ebner <f.ebner@proxmox.com>
2025-05-08 16:09:33 +0200

    block: mark bdrv_drained_begin() as GRAPH_UNLOCKED
    bdrv_drained_begin() polls and is not allowed to be called with the
    block graph lock held. Mark the function as such.
    
    Suggested-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

--- include/block/block-io.h
+++ include/block/block-io.h
@@ -429,7 +429,8 @@ bdrv_drain_poll(BlockDriverState *bs, BdrvChild *ignore_parent,
  *
  * This function can be recursive.
  */
-void bdrv_drained_begin(BlockDriverState *bs);
+void GRAPH_UNLOCKED
+bdrv_drained_begin(BlockDriverState *bs);
 
 /**
  * bdrv_do_drained_begin_quiesce:
