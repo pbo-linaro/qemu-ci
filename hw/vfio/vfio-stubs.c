137bc02fb95ff7ce195dca52f629b0f5f4db12fd
Steve Sistare <steven.sistare@oracle.com>
2025-06-10 08:39:33 -0700

    migration: close kvm after cpr
    cpr-transfer breaks vfio network connectivity to and from the guest, and
    the host system log shows:
      irq bypass consumer (token 00000000a03c32e5) registration fails: -16
    which is EBUSY.  This occurs because KVM descriptors are still open in
    the old QEMU process.  Close them.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>

---
+++ hw/vfio/vfio-stubs.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "qemu/osdep.h"
+#include "hw/vfio/vfio-device.h"
+
+void vfio_kvm_device_close(void)
+{
+    return;
+}
